b11fe4af198004ed1a253b88f6a2cee64304a593
Capitanski <felixzitzelsberger@yahoo.com>
2023-04-11 16:45:44 +0200

    Validated Test for Chapter 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    let final = '';
+    for (i = 0; i < num; i++) {
+        final += str
+    }
+    return final
 };
 
 // Do not edit below this line
