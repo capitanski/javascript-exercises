7ba4ff5dc82ba68d8325e6db9fe20f1e50bf4cd7
Capitanski <felixzitzelsberger@yahoo.com>
2023-04-11 17:29:54 +0200

    Completed Chapter 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (...args) {
+    let sum = 0;
+    let numbers = args.sort()
+    for (const arg of args) {
+        if (arg < 0) return 'ERROR'
+        if (typeof arg != 'number') return 'ERROR'
+    }
+    for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
