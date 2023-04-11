8f378b91fd9e1b707d514b8174bdf55f70d1c8fb
Capitanski <felixzitzelsberger@yahoo.com>
2023-04-11 17:19:16 +0200

    Completed Chapter 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    for (const num of args) {
+        const result = arr.findIndex((el) => el === num)
+        if (result >= 0) arr.splice(result, 1)
+    }
 
+
+    return arr
 };
 
 // Do not edit below this line
