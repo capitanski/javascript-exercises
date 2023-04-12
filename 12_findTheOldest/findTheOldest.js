8f825d0bcde471d2e06c9b03c1b94f3590ea54a5
Capitanski <felixzitzelsberger@yahoo.com>
2023-04-12 11:02:35 +0200

    Completed Chapter 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = people[0]
+    let age;
+    for (const person of people) {
+        if (!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear()
+        else {
+            age = person.yearOfDeath - person.yearOfBirth
+        }
 
+        if (age > oldest.yearOfDeath - oldest.yearOfBirth) {
+            oldest = person
+        }
+    }
+    return oldest
 };
 
 // Do not edit below this line
