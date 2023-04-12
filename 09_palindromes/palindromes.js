1cfdbf7c6cec862106d1dfbcdad4c079657b7ea4
Capitanski <felixzitzelsberger@yahoo.com>
2023-04-12 10:14:50 +0200

    Completed Chapter 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.split("")
+    arr = arr.filter((elem) => elem.toUpperCase() != elem.toLowerCase())
+    arr = arr.map((x) => x.toLowerCase())
+    arr = arr.join("")
+    let reversed = arr.split("").reverse().join("")
+    if (reversed == arr) return true
+    return false
 };
 
 // Do not edit below this line
