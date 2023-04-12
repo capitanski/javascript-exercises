11ec7789550d9e65017ab5cd67ee2ba560c4e0fd
Capitanski <felixzitzelsberger@yahoo.com>
2023-04-12 10:48:35 +0200

    Completed Chapter 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num < 0) return 'OOPS'
+    num = parseInt(num)
+    let arr = []
+    arr[0] = 1
+    arr[1] = 1
+    arr[2] = 2
+    for (let i = 2; i < num; i++) {
+        arr[i + 1] = arr[i] + arr[i - 1]
+    }
+    return arr[num - 1]
 };
 
 // Do not edit below this line
