4e0c6390e2bc7e6f8cd095afe7e387f2fd5c001d
Capitanski <felixzitzelsberger@yahoo.com>
2023-04-12 09:59:55 +0200

    Completed Chapter 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let result = 0
+
+  for (const num of arr) {
+    result += num
+  }
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let result = 0
+  for (const num of arr) {
+    if (result == 0) result = num
+    else {
+      result *= num
+    }
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  let result = Math.pow(x, y)
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  let result = 1
+  while (x > 0) {
+    result = result * (x)
+    x--
+  }
+  return result
 };
 
 // Do not edit below this line
